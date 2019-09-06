cbdfe96dfef522a2b37762098002f23bf87ea411
Raine Wallace <rainebeaudubs@gmail.com>
2019-09-06 11:23:21 -0400

    oop1 passes the tests, yay

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,27 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle:
+  def __init__(self):
+    pass
+  #this is the base class
+
+class FlightVehicle(Vehicle):
+  pass
+
+class Starship(FlightVehicle):
+  pass
+
+class GroundVehicle(Vehicle):
+  pass
+
+class Car(GroundVehicle):
+  pass
+
+class Motorcycle(GroundVehicle):
+  pass
+
+class Airplane(FlightVehicle):
+  pass
